7b6742801d8a26d9ff54071c28c678958f5beea2
VaniOFX <iebardarov@gmail.com>
2018-05-04 17:32:15 +0800

    a bit more maps

--- app/src/main/java/hk/ust/cse/comp4521/watsup/MapActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/watsup/MapActivity.java
@@ -2,12 +2,20 @@ package hk.ust.cse.comp4521.watsup;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -17,8 +25,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -26,6 +40,9 @@ public class MapActivity extends AppCompatActivity {
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISIONS_REQUEST_CODE = 1234;
     private static final float DEFAULT_ZOOM = 15f;
+
+    private EditText mSearchText;
+
     private boolean mLocationPermissonsGranted = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
@@ -34,10 +51,46 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mSearchText = (EditText) findViewById(R.id.inputSearch);
+
         getLocationPermission();
     }
 
-    private void initMap(){
+    private void init() {
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.KEYCODE_ENTER) {
+
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void geoLocate() {
+        Log.d(TAG, "Geolocating");
+        String searchString = mSearchText.getText().toString().trim();
+        Geocoder geocoder = new Geocoder(MapActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {
+
+        }
+        if (list.size() > 0) {
+            Address address = list.get(0);
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()),
+                    DEFAULT_ZOOM,
+                    address.getAddressLine(0) );
+        }
+    }
+
+    private void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
@@ -45,9 +98,13 @@ public class MapActivity extends AppCompatActivity {
                 Toast.makeText(MapActivity.this, "The map is loading", Toast.LENGTH_SHORT).show();
                 mMap = googleMap;
 
-                if(mLocationPermissonsGranted){
+                if (mLocationPermissonsGranted) {
                     getDeviceLocation();
-//                    mMap.setMyLocationEnabled(true);
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    mMap.setMyLocationEnabled(true);
+                    init();
                 }
             }
         });
@@ -56,13 +113,17 @@ public class MapActivity extends AppCompatActivity {
     private void getDeviceLocation(){
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         try{
+
             Task location =  mFusedLocationProviderClient.getLastLocation();
             location.addOnCompleteListener(new OnCompleteListener() {
                 @Override
                 public void onComplete(@NonNull Task task) {
                     if(task.isSuccessful()){
                         Location currentLocaiton = (Location) task.getResult();
-                        moveCamera(new LatLng(currentLocaiton.getLatitude(), currentLocaiton.getLongitude()), DEFAULT_ZOOM);
+                        moveCamera(new LatLng(currentLocaiton.getLatitude(),
+                                currentLocaiton.getLongitude()),
+                                DEFAULT_ZOOM,
+                                "My Locaiton");
                     }
                     else{
                         Toast.makeText(MapActivity.this, "Unable to find the locaton", Toast.LENGTH_LONG).show();
@@ -75,8 +136,17 @@ public class MapActivity extends AppCompatActivity {
         }
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
+    private void moveCamera(LatLng latLng, float zoom, String title){
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        if(!title.equals("My Location")){
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(title);
+            mMap.addMarker(options);
+        }
+
+
     }
 
     @Override
